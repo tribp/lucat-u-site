36a7c215af833c4b60b454c299a01e35087a4eff
Peter Triboutgit config --global user.email 28966023+tribp@users.noreply.github.com <28966023+tribp@users.noreply.github.com>
2022-12-01 10:10:39 +0100

    extended contact test for fetch api call inside swa api

--- api/contact/index.js
+++ api/contact/index.js
@@ -1,14 +1,18 @@
+const fetch = require('node-fetch')
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    const response = await fetch("https://www.boredapi.com/api/activity")
+    const activity = await response.json()
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    const mykey = process.env["PEXELS_API_KEY"]
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage + "key:" + mykey
+        body: activity
     };
 }
